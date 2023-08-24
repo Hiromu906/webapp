1673575e75a8195732db815e6fcf0979d8b20994
Hiromu906 <hiromu.s.0906@gmail.com>
2023-08-24 13:57:13 +0900

    初期設定&基礎実装

---
+++ database/migrations/2023_08_22_142933_create_shared_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shared_events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->unsignedInteger('shared_user_id')->references('id')->on('events')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shared_events');
+    }
+};
